2c882fd7eaed24a175084ad2a0c92a6ab9eeba59
kizenkov <kizenkov@tut.by>
2021-09-14 11:59:10 +0300

    resume

--- index.js
+++
@@ -1,9 +0,0 @@
-let phone = document.querySelector("#buttonPhone");
-let number = document.querySelector("#number");
-
-phone.addEventListener('click', showPhone);
-
-function showPhone() {
-    phone.remove();
-    number.innerHTML= '+ 375 33 695 08 99';
-}
