44b03630fea911c202edb919f5caae56fa362c7d
kizenkov <kizenkov@tut.by>
2021-09-15 10:27:32 +0300

    refactoring

---
+++ index.js
@@ -0,0 +1,21 @@
+document.getElementById('name').style.opacity = 1;
+
+setTimeout(() => {
+    document.getElementById('contacts').style.opacity = 1;
+}, 100)
+
+setTimeout(() => {
+document.getElementById('skills').style.opacity = 1;
+}, 200)
+
+setTimeout(() => {
+document.getElementById('projects').style.opacity = 1;
+}, 300)
+
+setTimeout(() => {
+document.getElementById('education').style.opacity = 1;
+}, 400)
+
+setTimeout(() => {
+document.getElementById('work').style.opacity = 1;
+}, 500)
