218f5f587d3f9a77e377211b855f486d1b90c65d
kizenkov <kizenkov@tut.by>
2021-10-13 12:34:14 +0300

    add cryptocurrencies

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-document.getElementById('name').style.opacity = 1;
+document.getElementById('name').style.opacity = '1';
 
 setTimeout(() => {
-    document.getElementById('contacts').style.opacity = 1;
+    document.getElementById('contacts').style.opacity = '1';
 }, 100)
 
 setTimeout(() => {
-document.getElementById('skills').style.opacity = 1;
+document.getElementById('skills').style.opacity = '1';
 }, 200)
 
 setTimeout(() => {
-document.getElementById('projects').style.opacity = 1;
+document.getElementById('projects').style.opacity = '1';
 }, 300)
 
 setTimeout(() => {
-document.getElementById('education').style.opacity = 1;
+document.getElementById('education').style.opacity = '1';
 }, 400)
 
 setTimeout(() => {
-document.getElementById('work').style.opacity = 1;
+document.getElementById('work').style.opacity = '1';
 }, 500)
